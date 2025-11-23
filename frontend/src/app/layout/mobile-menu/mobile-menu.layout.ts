de063581cb6fd247f15d9e092ca843f99952c938
zecarlosjr <BRMOU659987@ambev.com.br>
2025-11-23 20:07:56 -0300

    update ngif

--- frontend/src/app/layout/mobile-menu/mobile-menu.layout.ts
+++ frontend/src/app/layout/mobile-menu/mobile-menu.layout.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import {RouterLink, RouterOutlet} from '@angular/router';
-import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'mobile-menu',
@@ -9,7 +8,6 @@ import {NgIf} from '@angular/common';
   imports: [
     RouterOutlet,
     RouterLink,
-    NgIf
   ],
   styleUrls: ['./mobile-menu.layout.scss']
 })
